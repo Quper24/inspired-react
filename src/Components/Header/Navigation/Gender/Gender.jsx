1de9ee03730056a05b2bbf769bbe7675e9e28afa
Maksim Leskin <Maksim.leskin@methed.ru>
2023-06-25 14:58:36 +0300

    add rouer

--- src/Components/Header/Navigation/Gender/Gender.jsx
+++ src/Components/Header/Navigation/Gender/Gender.jsx
@@ -1,3 +1,18 @@
-export const Gender = () => (
-  <div>Gender</div>
-)
+import { NavLink } from "react-router-dom";
+import s from "./Gender.module.scss";
+import cn from "classnames";
+
+export const Gender = ({ list }) => (
+  <ul className={s.gender}>
+    {list.map((item) => (
+      <li key={item.link} className={s.item}>
+        <NavLink
+          className={({ isActive }) => cn(s.link, isActive && s.linkActive)}
+          to={item.link}
+        >
+          {item.title}
+        </NavLink>
+      </li>
+    ))}
+  </ul>
+);
