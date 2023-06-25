e446f317ea133689faac024eb026864968d6829d
Maksim Leskin <Maksim.leskin@methed.ru>
2023-06-25 21:43:46 +0300

    fetchNavigation

--- src/Components/Header/Navigation/Gender/Gender.jsx
+++ src/Components/Header/Navigation/Gender/Gender.jsx
@@ -1,18 +1,25 @@
 import { NavLink } from "react-router-dom";
 import s from "./Gender.module.scss";
 import cn from "classnames";
+import { useSelector } from 'react-redux';
 
-export const Gender = ({ list }) => (
-  <ul className={s.gender}>
-    {list.map((item) => (
-      <li key={item.link} className={s.item}>
-        <NavLink
-          className={({ isActive }) => cn(s.link, isActive && s.linkActive)}
-          to={item.link}
-        >
-          {item.title}
-        </NavLink>
-      </li>
-    ))}
-  </ul>
-);
+export const Gender = () => {
+  const {activeGender, genderList, categories } = useSelector(state => state.navigation)
+
+  return (
+    <ul className={s.gender}>
+      {genderList.map((gender) => (
+        <li key={gender} className={s.item}>
+          <NavLink
+            className={({ isActive }) => 
+              cn(s.link, (isActive || gender === activeGender) && s.linkActive)
+            }
+            to={gender}
+          >
+            {categories[gender].title}
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import 'normalize.css';
-import './index.scss'
-import { App } from './App.jsx';
+import "normalize.css";
+import "./index.scss";
+import { App } from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./store.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
