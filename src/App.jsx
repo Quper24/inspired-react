9f234832dc92d4222b21cb0df88e13e6223f7eec
Maksim Leskin <Maksim.leskin@methed.ru>
2023-06-28 19:51:18 +0300

    mainpage

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { ErrorPage } from './Components/ErrorPage/ErrorPage.jsx';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchNavigation } from './features/navigationSlice.js';
+import { fetchColors } from './features/colorSlice.js';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -17,8 +18,10 @@ const router = createBrowserRouter(
       <Route index element={<MainPage />} />
       <Route path="women" element={<MainPage gender="women" />} />
       <Route path="men" element={<MainPage gender="men" />} />
+      <Route path="kids" element={<MainPage gender="kids" />} />
       <Route path="women/:category" element={<MainPage gender="women" />} />
       <Route path="men/:category" element={<MainPage gender="men" />} />
+      <Route path="kids/:category" element={<MainPage gender="kids" />} />
       <Route path="*" element={<ErrorPage />} />
     </Route>
   )
@@ -29,6 +32,7 @@ export const App = () => {
 
   useEffect(() => {
     dispatch(fetchNavigation());
+    dispatch(fetchColors());
   }, [dispatch]);
 
   return <RouterProvider router={router}></RouterProvider>;
