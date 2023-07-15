32a6ef2dd4e491555ffc6ebf41b8ae9318513b42
Leskin Maksim <Quper2405@gmail.com>
2023-07-15 18:42:38 +0300

    add scrollTop

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 import { Header } from "../Components/Header/Header.jsx";
 import { Main } from "../Components/Layout/Main/Main.jsx";
 import { Footer } from "../Components/Footer/Footer.jsx";
+import ScrollToTop from '../ScrollTop.js';
 
 
 export const Root = () => (
   <>
+    <ScrollToTop />
     <Header />
     <Main>
       <Outlet />
